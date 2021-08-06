e31056b8e339879d2bb39a90f12069b343f14830
Ayala Gottfried <86181688+AyalaGottfried@users.noreply.github.com>
2021-08-06 11:35:16 +0300

    Update get_char_index.py

--- get_char_index.py
+++ get_char_index.py
@@ -8,3 +8,16 @@ def get_char_index(char):
     elif 122 >= ord(char) >= 97:
         return ord(char) - 97
     return -1
+
+def remove_dup(lst):
+    new_lst = []
+    for i in lst:
+        added = False
+        for j in range(len(new_lst)):
+            if i[0] == new_lst[j][0]:
+                new_lst[j] = max(i, new_lst[j], key=lambda s: s[1])
+                added = True
+                break
+        if not added:
+            new_lst.append(i)
+    return new_lst
