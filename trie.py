c7bc3f577dd76ac4c4c2208710abe065f619a9b6
Ayala Gottfried <86181688+AyalaGottfried@users.noreply.github.com>
2021-08-06 11:35:34 +0300

    Update trie.py

--- trie.py
+++ trie.py
@@ -1,4 +1,4 @@
-from get_char_index import get_char_index
+from get_char_index import get_char_index, remove_dup
 
 
 class Trie:
@@ -9,6 +9,7 @@ class Trie:
 
     def __init__(self):
         self.__nodes = [0 for i in range(27)]
+        self.self.__founded_sentences = []
 
     def add_sentence(self, sentence, file_index, sentence_index):
         """
@@ -55,15 +56,15 @@ class Trie:
         :return: a list with the five high scored sentences that contain the substring.
         """
         node = self.__nodes
-        sentences = []
-        self.req_search(node, substring, 0, 0, sentences)
-        if len(sentences) < 5:
-            self.repair_search(node, substring, 0, 0, sentences)
-        if len(sentences) < 5:
-            self.small_indexes_repair(node, substring, 0, sentences)
-        return sentences
+        self.__founded_sentences = []
+        self.req_search(node, substring, 0, 0)
+        if len(self.__founded_sentences) < 5:
+            self.repair_search(node, substring, 0, 0)
+        if len(self.__founded_sentences) < 5:
+            self.small_indexes_repair(node, substring, 0)
+        return self.__founded_sentences
 
-    def small_indexes_repair(self, node, substring, index, sentences):
+    def small_indexes_repair(self, node, substring, index):
         """
         Searches a substring with repairs on its small indexes.
         :param node: a node in the trie where the searches on
@@ -73,17 +74,17 @@ class Trie:
         """
         for i in range(4, -1, -1):
             for j in range(26):
-                self.req_search(node, substring[:i] + chr(j + 97) + substring[i + 1:], index, 5 - i, sentences)
-                if len(sentences) >= 5:
+                self.req_search(node, substring[:i] + chr(j + 97) + substring[i + 1:], index, 5 - i)
+                if len(self.__founded_sentences) >= 5:
                     return
         for i in range(4, -1, -1):
-            self.req_search(node, substring[:i] + substring[i + 1:], index, 10 - 2 * i, sentences)
+            self.req_search(node, substring[:i] + substring[i + 1:], index, 10 - 2 * i)
             for j in range(26):
-                self.req_search(node, substring[:i] + chr(j + 97) + substring[i:], index, 10 - 2 * i, sentences)
-                if len(sentences) >= 5:
+                self.req_search(node, substring[:i] + chr(j + 97) + substring[i:], index, 10 - 2 * i)
+                if len(self.__founded_sentences) >= 5:
                     return
 
-    def repair_search(self, node, substring, index, was_repair, sentences):
+    def repair_search(self, node, substring, index, was_repair):
         """
         Searches a substring with repairs
         :param node:  a node in the trie where the searches on
@@ -96,10 +97,11 @@ class Trie:
             return
         if len(substring) == 0 and was_repair:
             if len(node) > 27:
-                sentences += map(lambda s: (s, index * 2 - was_repair), node[27])
-            if len(sentences) >= 5:
+                self.__founded_sentences += map(lambda s: (s, index * 2 - was_repair), node[27])
+                self.__founded_sentences = remove_dup(self.__founded_sentences)
+            if len(self.__founded_sentences) >= 5:
                 return
-            self.get_all_children(node, index, was_repair, sentences)
+            self.get_all_children(node, index, was_repair)
             return
         if len(substring) > 0:
             c = 0
@@ -107,7 +109,7 @@ class Trie:
             while char_index == -1:
                 c += 1
                 if c == len(substring):
-                    self.get_all_children(node, index, was_repair, sentences)
+                    self.get_all_children(node, index, was_repair)
                     return
                 char_index = get_char_index(substring[c])
             while c < len(substring) - 1:
@@ -118,25 +120,23 @@ class Trie:
                 else:
                     break
             if was_repair or index < 4:
-                self.repair_search(node[char_index], substring[c + 1:], index + 1, was_repair, sentences)
+                self.repair_search(node[char_index], substring[c + 1:], index + 1, was_repair)
                 return
             else_decrease = 10 - 2 * index if index < 5 else 2
-            self.repair_search(node, substring[c + 1:], index + 1, else_decrease, sentences)
-            if len(sentences) >= 5:
+            self.repair_search(node, substring[c + 1:], index + 1, else_decrease)
+            if len(self.__founded_sentences) >= 5:
                 return
             for i in range(26):
-                self.repair_search(node[char_index], chr(i + 97) + substring[c + 1:], index + 1, else_decrease,
-                                   sentences)
-                if len(sentences) >= 5:
+                self.repair_search(node[char_index], chr(i + 97) + substring[c + 1:], index + 1, else_decrease)
+                if len(self.__founded_sentences) >= 5:
                     return
-                self.repair_search(node[char_index], ' ' + substring[c + 1:], index + 1, else_decrease,
-                                   sentences)
-                if len(sentences) >= 5:
+                self.repair_search(node[char_index], ' ' + substring[c + 1:], index + 1, else_decrease)
+                if len(self.__founded_sentences) >= 5:
                     return
-            self.repair_search(node[char_index], substring[c + 1:], index + 1, 0, sentences)
+            self.repair_search(node[char_index], substring[c + 1:], index + 1, 0)
         return
 
-    def req_search(self, node, substring, index, was_repair, sentences):
+    def req_search(self, node, substring, index, was_repair):
         """
         Searches for a substring in a node recursively
         :param node: the current node
@@ -150,17 +150,18 @@ class Trie:
             return was_repair
         if len(substring) == 0:
             if len(node) > 27:
-                sentences += map(lambda s: (s, index * 2 - was_repair), node[27])
-            if len(sentences) >= 5:
+                self.__founded_sentences += map(lambda s: (s, index * 2 - was_repair), node[27])
+                self.__founded_sentences = remove_dup(self.__founded_sentences)
+            if len(self.__founded_sentences) >= 5:
                 return was_repair
-            self.get_all_children(node, index, was_repair, sentences)
-            return sentences, was_repair
+            self.get_all_children(node, index, was_repair)
+            return was_repair
         c = 0
         char_index = get_char_index(substring[c])
         while char_index == -1:
             c += 1
             if c == len(substring):
-                self.get_all_children(node, index, was_repair, sentences)
+                self.get_all_children(node, index, was_repair)
                 return was_repair
             char_index = get_char_index(substring[c])
         while c < len(substring) - 1:
@@ -170,18 +171,18 @@ class Trie:
                 char_index = get_char_index(substring[c])
             else:
                 break
-        self.req_search(node[char_index], substring[c + 1:], index + 1, was_repair, sentences)
-        if len(sentences) >= 5:
+        self.req_search(node[char_index], substring[c + 1:], index + 1, was_repair)
+        if len(self.__founded_sentences) >= 5:
             return was_repair
         if not was_repair and index > 3:
             replace_decrease = 5 - index if index < 5 else 1
             for i in range(27):
-                self.req_search(node[i], substring[c + 1:], index + 1, replace_decrease, sentences)
-                if len(sentences) >= 5:
+                self.req_search(node[i], substring[c + 1:], index + 1, replace_decrease)
+                if len(self.__founded_sentences) >= 5:
                     return replace_decrease
         return was_repair
 
-    def get_all_children(self, node, index, repair, sentences):
+    def get_all_children(self, node, index, repair):
         """
         Adds to sentences up to five sentences from the leaves of a specific node
         :param node: a node to add its leaves to sentences
@@ -189,13 +190,14 @@ class Trie:
         :param repair: the cost of the repair in the substring that was searched
         :param sentences: a list with the matches sentences
         """
-        if (len(sentences) >= 5 and repair == 0) or node == 0:
+        if (len(self.__founded_sentences) >= 5 and repair == 0) or node == 0:
             return
         if len(node) > 27:
-            sentences += map(lambda s: (s, index * 2 - repair), node[27])
-            if len(sentences) >= 5:
+            self.__founded_sentences += map(lambda s: (s, index * 2 - repair), node[27])
+            self.__founded_sentences = remove_dup(self.__founded_sentences)
+            if len(self.__founded_sentences) >= 5:
                 return
         for i in range(27):
-            self.get_all_children(node[i], index, repair, sentences)
-            if len(sentences) >= 5:
+            self.get_all_children(node[i], index, repair)
+            if len(self.__founded_sentences) >= 5:
                 return
